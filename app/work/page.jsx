9d1fed18604e351c51f4b1b2ef85b5eb302798fe
Juan Jose Londono <jjtabares@sysman.com.co>
2024-08-11 21:34:27 -0500

    work to Work

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -62,7 +62,7 @@ const projects = [
   },
 ];
 
-const work = () => {
+const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
   const handleSlideChange = (swiper) => {
@@ -150,4 +150,4 @@ const work = () => {
   );
 };
 
-export default work;
+export default Work;
